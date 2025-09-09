6369914863e99b665e77b7fa714e915e95c1125f
Bantita Rattanajutamanee <bantita.rat@ku.th>
2025-09-09 11:55:09 +0700

    week7

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -22,3 +22,21 @@ class Rectangle:
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
+
+
+
+class Circle:
+
+    def _init__(self, radius):
+        self.radius = radius
+        print("Circle created!")
+    
+    def getAre(self):
+        return 3.1416 * self.radius * self.radius
+    
+    def getPerimeter(self):
+        return 2 * 3.1416 * self. radius
+    
+myCircle = Circle(10)
+print(myCircle. getArea())
+print(nyCircle.getPerimeter))
