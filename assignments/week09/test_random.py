cbf617a8996bfe7ad3a32cf4309a6244961e0385
Bantita Rattanajutamanee <bantita.rat@ku.th>
2025-09-23 11:50:59 +0700

    week9

--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,24 @@
 import random
 
+#refactor != debug
+
 def test_random():
+    #สุ่มเลขระหว่าง1-10 เก็บไว้ในตัวเเปรชื่อrandom_number
     random_number = random.randint(1, 100)
+
+    #รับค่าการทายเลขจากผู้ใช้ เก็บไว้ในตัวแปรชื่อ gurss_number
+    guess_number = input("What is your guess number?: ")
+    #guess_number = int(guess_number)จะเขียนแบบนี้ก็ได้
+
+    if random_number == guess_number:
+        print("Congratulation")
+
+    elif random_number < guess_number:
+        print("Too much")
+
+    elif random_number > guess_number:
+        print("Too low")
+
     print(random_number)
     
 test_random()
